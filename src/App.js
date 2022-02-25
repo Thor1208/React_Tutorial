4d6b11ed0b5f2cb04446d3e6777af8fd1158ec19
Thor1208 <97014168+Thor1208@users.noreply.github.com>
2022-02-25 16:38:11 +0530

    ....

--- src/App.js
+++ src/App.js
@@ -5,10 +5,20 @@ import Welcome from './components/Welcome'
 import Hello from './components/Hello';
 import Message from './components/Message';
 import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
+import EventBind from './components/EventBind';
+import ParentComponent from './components/ParentComponent';
+import UserGreeting from './components/UserGreeting';
 
 function App() {
   return (
     <div className="App">
+      <UserGreeting></UserGreeting>
+      {/* <ParentComponent></ParentComponent> */}
+      {/* <EventBind></EventBind> */}
+      {/* <FunctionClick></FunctionClick> */}
+      {/* <ClassClick></ClassClick> */}
       {/* <Counter></Counter> */}
       {/* <Message></Message> */}
       {/* <Greet name="Vijay" heroName="Thor">
@@ -17,8 +27,8 @@ function App() {
       {/* <Greet name="Pratik" heroName="IronMan">
         <button>Action</button>
       </Greet> */}
-      <Greet name="Aaron" heroName="Spiderman"></Greet>
-      <Welcome name="Vijay" heroName="Thor"></Welcome>
+      {/* <Greet name="Aaron" heroName="Spiderman"></Greet> */}
+      {/* <Welcome name="Vijay" heroName="Thor"></Welcome> */}
       {/* <Welcome name="Pratik" heroName="IronMan"></Welcome> */}
       {/* <Welcome name="Aaron" heroName="Spiderman"></Welcome> */}
       {/* <Hello></Hello> */}
